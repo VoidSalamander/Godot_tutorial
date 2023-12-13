246e67abfd2deeba0a3bd68a9b887f65d99c0f4b
VoidSalamander <VoidSalamander1252@gmail.com>
2023-12-14 03:13:28 +0800

    make game work
    it can work now

--- main/MobGenerator.gd
+++ main/MobGenerator.gd
@@ -6,12 +6,10 @@ extends Node2D
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
-
+	
 func _on_mob_spawn_timer_timeout():
 	var mob = mob_node.instantiate()
 	mob.player = get_parent().get_node("player")
---
+++ main/TileMap.gd
@@ -0,0 +1,18 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func get_tile_data(position: Vector2, data_name: String) -> Variant:
+	var tile_position = self.local_to_map(position)
+	var data: TileData = self.get_cell_tile_data(0, tile_position)
+	if data:
+		return data.get_custom_data(data_name)
+	return null
