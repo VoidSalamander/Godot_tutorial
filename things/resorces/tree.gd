006efc2c8f88905472003bf1ea257e4569f7ef7a
VoidSalamander <VoidSalamander1252@gmail.com>
2023-12-27 07:48:20 +0800

    Revert "stone and tree"
    This reverts commit ccb1a2850b33f52580937a388334a3ec0c6702b5.

--- things/resorces/tree.gd
+++ things/resorces/tree.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-@onready var item_list_node = get_parent().get_node("item_list")
-
-@export var wood : PackedScene
-
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		$TouchScreenButton.visible = true
@@ -12,7 +8,7 @@ func _on_area_2d_body_exited(body):
 	if body.is_in_group("player"):
 		$TouchScreenButton.visible = false
 		
-func _on_touch_screen_button_pressed():	
+func _on_touch_screen_button_pressed():
 	$TextureProgressBar.visible = true
 
 	# Loop to decrease the progress bar value gradually
@@ -28,17 +24,6 @@ func _on_touch_screen_button_pressed():
 	await get_tree().create_timer(1).timeout
 	$wood/AnimationPlayer.play("fade")
 	await get_tree().create_timer(0.5).timeout
-	
-	for i in range(2):
-		creat_item(wood)
-		
 	queue_free()
+
 	
-
-
-func creat_item(item: PackedScene):
-	var created_item = item.instantiate()
-	created_item.init_item($wood.global_position)
-	item_list_node.call_deferred("add_child", created_item)
-
-
